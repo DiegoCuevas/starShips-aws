a041e5cbd1b293de0fad6d7ad498e2ae1f0b1aa1
Diego Cuevas <diegocuevas579@gmail.com>
2024-10-13 17:56:51 -0500

    Add characters endpoint

---
+++ handlers/characters.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+const customParams = (personaje) => ({
+  nombre: personaje.name,
+  altura: personaje.height,
+  peso: personaje.mass,
+  colorCabello: personaje.hair_color,
+  colorPiel: personaje.skin_color,
+  colorOjos: personaje.eye_color,
+  añoNacimiento: personaje.birth_year,
+  género: personaje.gender,
+});
+
+export const getCharacters = async (event) => {
+  try {
+    const response = await axios.get('https://swapi.py4e.com/api/people/');
+    const characters = response.data.results.map(customParams);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(characters),
+    };
+  } catch (error) {
+    console.error('Error al obtener personajes:', error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ mensaje: 'Error al obtener personajes' }),
+    };
+  }
+};
